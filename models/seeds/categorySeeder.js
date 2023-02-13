5c9cb4191bc74156abbbcd93c58e2bdc8357f17d
abbie930 <abbiecheng930@gmail.com>
2023-02-13 16:37:51 +0800

    feat: add category seeder

---
+++ models/seeds/categorySeeder.js
@@ -0,0 +1,30 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const Category = require('../category')
+const db = require('../../config/mongoose')
+
+
+const SEED_CATEGORY = {
+  家居物業: 'fa-solid fa-house',
+  交通出行: 'fa-solid fa-van-shuttle',
+  休閒娛樂: 'fa-solid fa-face-grin-beam',
+  餐飲食品: 'fa-solid fa-utensils',
+  其他: 'fa-solid fa-pen',
+}
+
+db.once('open', async () => {
+  try {
+    for (const [name, icon] of Object.entries(SEED_CATEGORY)) {
+      await Category.create({
+        name,
+        icon,
+      })
+    }
+    console.log('done')
+    process.exit()
+  } catch (err) {
+    console.log(err)
+  }
+})
