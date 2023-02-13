cac5fb5e7a8c8b57ad8fb60b99471cb903869be8
abbie930 <abbiecheng930@gmail.com>
2023-02-13 14:10:40 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 
-const app = express()
-const port = 3000
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+const app = express()
+const port = 3000
+
+
+app.engine('handlebars', engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.set('views', './views')
 
 app.get('/', (req, res) => {
-  res.send(`This is a Express Web App`)
+  res.render('index')
 })
 
 
