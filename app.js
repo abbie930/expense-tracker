64e1bd3d48b93a31e91f8ce1365c075564876f06
abbie930 <abbiecheng930@gmail.com>
2023-02-13 13:33:38 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
+
 const app = express()
 const port = 3000
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 
 app.get('/', (req, res) => {
