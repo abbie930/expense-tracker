9e69d836ce7e04b5fbbc389b1ede2da4710cb30e
abbie930 <abbiecheng930@gmail.com>
2023-02-14 19:17:18 +0800

    feat: add recordList model

---
+++ models/recordList.js
@@ -0,0 +1,49 @@
+const users = [
+  {
+    name: '廣志',
+    email: 'user1@example.com',
+    password: '12345678',
+  },
+  {
+    name: '小新',
+    email: 'user2@example.com',
+    password: '12345678',
+  },
+]
+
+
+const records = [
+  {
+    name: '房租',
+    date: '2023-02-13',
+    amount: 10000,
+    category: '家居物業',
+  },
+  {
+    name: 'Uber',
+    date: '2023-02-14',
+    amount: 250,
+    category: '交通出行',
+  },
+  {
+    name: '唱歌',
+    date: '2023-02-15',
+    amount: 700,
+    category: '休閒娛樂',
+  },
+  {
+    name: '星巴克',
+    date: '2023-02-16',
+    amount: 200,
+    category: '餐飲食品',
+  },
+  {
+    name: '保險',
+    date: '2023-02-17',
+    amount: 4500,
+    category: '其他',
+  },
+]
+
+
+module.exports = { users, records }
