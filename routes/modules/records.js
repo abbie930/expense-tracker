a24e18d87de4e7733acbf32764d4baf5f637bf30
abbie930 <abbiecheng930@gmail.com>
2023-02-15 23:41:38 +0800

    feat: add create record function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
+const Category = require('../../models/category')
 
 
-
-router.get('/new', (req, res) => {
-  return res.render('new')
+//get new record page
+router.get('/new', async (req, res) => {
+  const categories = await Category.find().lean()
+  return res.render('new', { categories })
 })
+
+//post new record
+router.post('/', async (req, res) => {
+  try {
+    const mockUserId = "63eba5ab576f32517f1e58d4"
+    const { name, date, categoryId, amount } = req.body
+    await Record.create({
+      name,
+      date,
+      categoryId,
+      amount,
+      userId: mockUserId,
+    })
+    res.redirect('/')
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+//
 
 module.exports = router
