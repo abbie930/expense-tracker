ecf4c2ef38ae975190e3525b284f2014b1beeede
abbie930 <abbiecheng930@gmail.com>
2023-02-15 17:05:32 +0800

    feat: add records router

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
+const records = require('./modules/records')
 
 router.use('/', home)
+router.use('/records', records)
 
 module.exports = router
