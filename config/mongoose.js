800126cf1790d7904f028800023709394e06e283
abbie930 <abbiecheng930@gmail.com>
2023-02-16 20:47:11 +0800

    feat: add useFindAndModify option set

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+})
 
 
 const db = mongoose.connection
