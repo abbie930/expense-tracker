797fdf4967c803238e61481066369bab4045df3e
abbie930 <abbiecheng930@gmail.com>
2023-02-17 23:13:04 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 const User = require('../../models/user')
 
@@ -11,7 +12,8 @@ router.get('/login', (req, res) => {
 
 // add middleware to verify request login status
 router.post(
-  '/login', (req, res ,next) => {
+  '/login',
+  (req, res, next) => {
     const { email, password } = req.body
     if (!email || !password) {
       req.flash('warning_msg', '請輸入 email 和密碼')
@@ -26,53 +28,57 @@ router.post(
   })
 )
 
+// get register Page
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+
 // register verify
 router.post('/register', async (req, res) => {
-  try { 
-   const { name, email, password, confirmPassword } = req.body
-   const errors = []
-   if (!name || !email || !password || !confirmPassword) {
-     errors.push({ message: '所有欄位都是必填。' })
-   }
-   if (password !== confirmPassword) {
-     errors.push({ message: '密碼與確認密碼不相符！' })
-   }
-   if (errors.length) {
-     return res.render('register', {
-       errors,
-       name,
-       email,
-       password,
-       confirmPassword,
-     })
-   }
-   const user = await User.findOne({ email })
-   if (user) {
-     errors.push({ message: '這個 Email 已經註冊過了。' })
-     res.render('register', {
-       errors,
-       name,
-       email,
-       password,
-       confirmPassword,
-     })
-   } else {
-     await User.create({
-       name,
-       email,
-       password,
-     })
-     res.redirect('/')
-   }
+  try {
+    const { name, email, password, confirmPassword } = req.body
+    const errors = []
+    if (!name || !email || !password || !confirmPassword) {
+      errors.push({ message: '所有欄位都是必填。' })
+    }
+    if (password !== confirmPassword) {
+      errors.push({ message: '密碼與確認密碼不相符！' })
+    }
+    if (errors.length) {
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword,
+      })
+    }
+    const user = await User.findOne({ email })
+    if (user) {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword,
+      })
+    } else {
+      const salt = await bcrypt.genSalt(10)
+      const hashedPassword = await bcrypt.hash(password, salt)
+      await User.create({
+        name,
+        email,
+        password: hashedPassword,
+      })
+      res.redirect('/')
+    }
   } catch (err) {
     console.log(err)
   }
 })
 
-// get register Page
-router.get('/register', (req, res) => {
-  res.render('register')
-})
 
 // logout
 router.get('/logout', (req, res) => {
@@ -80,6 +86,5 @@ router.get('/logout', (req, res) => {
   req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
-
 
 module.exports = router
