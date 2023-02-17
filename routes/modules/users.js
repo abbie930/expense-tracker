532456a1656b5a04be9d827cf403f2e816d5336a
abbie930 <abbiecheng930@gmail.com>
2023-02-17 16:08:19 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,8 +3,16 @@ const router = express.Router()
 
 const User = require('../../models/user')
 
+//get login page
 router.get('/login', (req, res) => {
   res.render('login')
 })
+
+
+//login router
+router.post('/login', (req, res) => {
+
+})
+
 
 module.exports = router
