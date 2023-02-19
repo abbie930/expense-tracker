7de2e55e0e106eea7de1f732a682efc3b5553f8c
abbie930 <abbiecheng930@gmail.com>
2023-02-19 12:37:06 +0800

    feat: removed unused code

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -26,20 +26,15 @@ db.once('open', async () => {
           email,
           password: hashedPassword,
         })
-        console.log('users created')
 
         // assign corresponding record for each user(record: 1,2,3, => user1, record: 4, 5 => user2)
         const recordList = user_index ? records.slice(3) : records.slice(0, 3)
-        console.log('1', records.slice(3))
-        console.log('2', records.slice(0, 3))
         await Promise.all(
           recordList.map(async (record) => {
-            console.log('record', record)
             // get the value of the record
             const { name, date, amount, category } = record
             // Find the corresponding category id according to the category name
             const categoryData = categoriesFromDB.find((data) => data.name === category)
-            console.log('categoryData', categoryData)
             // Integrate the category id and user id into the record
             await Record.create({
               name,
