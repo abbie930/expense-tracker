24cfe65f59eaa88d177c48b925767d1a0ec747a0
abbie930 <abbiecheng930@gmail.com>
2023-02-19 13:30:37 +0800

    feat: remove unused doc

--- config/handlebars-helpers.js
+++
@@ -1,8 +0,0 @@
-const Handlebars = require('handlebars')
-
-Handlebars.registerHelper('ifSelect', function (a, b, options) {
-  if (String(a) === String(b)) {
-    return options.fn(this)
-  }
-  return options.inverse(this)
-})
